46238b8b89ca4ac48e17fcb4a4aa3e93ab384542
Aleksandr Uriumtsev <aleksandr.uriumtsev@tesco.com>
2023-02-11 01:19:36 +0300

    docker init

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv()
 
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
