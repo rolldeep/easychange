2703e6d732640963c01a290b4345679298c6197d
Aleksandr Uriumtsev <aleksandr.uriumtsev@tesco.com>
2023-02-18 00:04:22 +0300

    fix read pair

--- backend/app/db/crud.py
+++ backend/app/db/crud.py
@@ -1,12 +1,13 @@
 from sqlalchemy.orm import Session
+from typing import List
 
 from . import models
 
 
-def get_pair(db: Session, name: str):
+def get_pair(db: Session, name: str) -> models.Pair:
     return db.query(models.Pair).filter(models.Pair.name == name).first()
 
 
-def get_pairs(db: Session, skip: int = 0, limit: int = 100):
+def get_pairs(db: Session, skip: int = 0, limit: int = 100) -> List[models.Pair]:
     return db.query(models.Pair).offset(skip).limit(limit).all()
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from app.model.schemas import Pair
 from app.db import crud
+import uvicorn
 from app.db.database import SessionLocal, engine, get_db
 
 
@@ -15,9 +16,9 @@ def read_pairs(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return items
 
 
-@app.get("/pairs/{pair_id}", response_model=Pair)
-def read_user(pair_id: int, db: Session = Depends(get_db)):
-    db_pair = crud.get_pair(db, pair_id=pair_id)
+@app.get("/pairs/{name}", response_model=Pair)
+def read_pair(name: str, db: Session = Depends(get_db)):
+    db_pair = crud.get_pair(db=db, name=name)
     if db_pair is None:
         raise HTTPException(status_code=404, detail="Pair not found")
     return db_pair
@@ -26,3 +27,7 @@ def read_user(pair_id: int, db: Session = Depends(get_db)):
 def get_home(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     items = crud.get_pairs(db, skip=skip, limit=limit)
     return items
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
