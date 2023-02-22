28226c48b1a0ccf2c26b603d6b176cfb99e1256f
Aleksandr Uriumtsev <aleksandr.uriumtsev@tesco.com>
2023-02-22 22:58:58 +0100

    form fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import NewExpense from './components/NewExpense/NewExpense';
+import { useState } from 'react';
+
+const DATA = [];
 
 function App() {
+
+  const [expenses, setExpenses] = useState(DATA);
+
+  const submitExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+    console.log(expenses);
+  };
+
   return (
     <div className="App">
-      
+      <NewExpense onAddExpense={submitExpenseHandler} />
     </div>
   );
 }
